b6416861c922d917b7fd5fa1827d5dce71f9192c
CePseudo <hoffmann.axel.6032@gmail.com>
2025-11-11 18:16:14 +0100

    seeders

--- apps/api/database/seeders/main/index_seeder.ts
+++ apps/api/database/seeders/main/index_seeder.ts
@@ -1,19 +1,6 @@
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
-import app from '@adonisjs/core/services/app'
 
 export default class extends BaseSeeder {
-  private async seed(Seeder: { default: typeof BaseSeeder }) {
-    /**
-     * Do not run when not in development mode and seeder
-     * is not explicitly running in production
-     */
-    if (!app.inDev && !BaseSeeder.environment) {
-      return
-    }
-
-    await new Seeder.default(this.client).run()
-  }
-
   async run() {
     await this.seed(await import('#database/seeders/0_role_seeder'))
     await this.seed(await import('#database/seeders/2_genre_seeder'))
@@ -22,9 +9,10 @@ export default class extends BaseSeeder {
     await this.seed(await import('#database/seeders/5_article_category_seeder'))
     await this.seed(await import('#database/seeders/8_guide_type_seeder'))
     await this.seed(await import('#database/seeders/9_difficulty_level_seeder'))
-    await this.seed(await import('#database/seeders/6_game_seeder'))
-    await this.seed(await import('#database/seeders/7_article_seeder'))
-    await this.seed(await import('#database/seeders/10_youtube_video_seeder'))
     await this.seed(await import('#database/seeders/11_user_auth_seeder'))
+  }
+
+  private async seed(Seeder: { default: typeof BaseSeeder }) {
+    await new Seeder.default(this.client).run()
   }
 }
